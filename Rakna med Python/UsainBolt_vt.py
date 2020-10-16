514404d217d88a9fe4e1026cf691d62dbc2abff3
Kokchun <69901510+kokchungiang@users.noreply.github.com>
2020-10-16 09:22:30 +0200

    usain bolt vt

--- Rakna med Python/UsainBolt_vt.py
+++ Rakna med Python/UsainBolt_vt.py
@@ -5,12 +5,13 @@ t = [0, 1.83, 2.87, 3.78, 4.65, 5.5, 6.32, 7.14, 7.96, 8.79, 9.69]
 v = [0]  # tom lista
 
 for i in range(1, len(t)):
-    v.append((s[i]-s[i-1])/(t[i]-t[i-1]))
-    print(i)
-
-print(len(v))
-print(len(t))
+    v_i = (s[i]-s[i-1])/(t[i]-t[i-1])
+    v.append(v_i)  # lägger till v_i till listan
 
 # plot v-t graf
 plt.plot(t, v, '*-')
+plt.title("Usain Bolt 100m - v-t graf")
+plt.xlabel("Tiden i sekunder")
+plt.ylabel("Hastigheten i m/s")
+plt.grid()
 plt.show()
